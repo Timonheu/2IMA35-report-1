44285ce1024da0a691ce947005b51825ec3d4cf4
Timon Heuwekemeijer <timonheu@gmail.com>
2022-12-01 11:55:19 +0100

    kleine refactor en comments

--- twitterScraper.py
+++ twitterScraper.py
@@ -1,27 +1,29 @@
 import tweepy
 
-# OAuth Authentication
+# Get the API keys
 exec(open("twitter.conf").read())
 
-# auth = tweepy.OAuthHandler(apiKey, apiSecretKey)
-# auth.set_access_token(accessToken, accessTokenSecret)
-# api = tweepy.API(auth)
+# authenticate
 client = tweepy.Client(bearerToken)
 
+# people for which we will collect the people they follow
 handleList = ["thierrybaudet"]
 
 for handle in handleList:
     user = client.get_user(username=handle)
 
-    followerResponse = client.get_users_following(user.data.id, max_results=1000)
-    followers = []
-    for data in followerResponse.data:
-        followers.append(data.username)
-    if len(followers) == 1000:
+    # API response contaning the people followed by this politician
+    followedResponse = client.get_users_following(user.data.id, max_results=1000)
+    followed = []
+    # Add the username of each followed person to the list
+    for data in followedResponse.data:
+        followed.append(data.username)
+    if len(followed) == 1000:
+        # This means the limit of our request was reached and the politician follows at least 1000 people
         print("AAAH " + handle + " volgt te veel mensen")
 
     # list of indexes in handleList of people that this person follows
     indexList = []
-    for follower in followers:
-        if follower in handleList:
-            index = handleList.index(follower)
+    for politician in followed:
+        if politician in handleList:
+            index = handleList.index(politician)
