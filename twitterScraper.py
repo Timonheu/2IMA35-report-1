ac6453ffcb3ba6f2bcf19b1ca302ed0bd846b289
Timon Heuwekemeijer <timonheu@gmail.com>
2022-12-01 11:45:36 +0100

    lijst van indexen (moet nog aangepast worden op de JSON)

--- twitterScraper.py
+++ twitterScraper.py
@@ -13,10 +13,15 @@ handleList = ["thierrybaudet"]
 for handle in handleList:
     user = client.get_user(username=handle)
 
-    followers = client.get_users_following(user.data.id, max_results=1000)
-    counter = 0
-    for data in followers.data:
-        print(data.username)
-        counter += 1
-    if counter == 1000:
-        print("AAAH " + handle + "volgt te veel mensen")
+    followerResponse = client.get_users_following(user.data.id, max_results=1000)
+    followers = []
+    for data in followerResponse.data:
+        followers.append(data.username)
+    if len(followers) == 1000:
+        print("AAAH " + handle + " volgt te veel mensen")
+
+    # list of indexes in handleList of people that this person follows
+    indexList = []
+    for follower in followers:
+        if follower in handleList:
+            index = handleList.index(follower)
